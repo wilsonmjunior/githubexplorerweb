7e6c21198438c5d560e80aba81d9460b51e9558b
Wilson Matokanovic Jubior <wilsonmjuniorx@gmail.com>
2026-08-01 01:48:38 -0300

    chore: remove unnecessary config comments

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
